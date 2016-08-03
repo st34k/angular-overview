66ab369e85df34da4979b563fceb31d9f0bf5eee
Sashko <Sashko>
2016-08-03 17:21:10 +0300

    fixed display bugs added search by year

--- anguflix/controller.js
+++ anguflix/controller.js
@@ -1,13 +1,26 @@
 
 app.controller('movieCtrl', function($scope, movieService, personalCollectionService, yearService){
-
+  $scope.show = true;
   $scope.allMovies = movieService.allMovies;
+
+  
 
   $scope.addToCollection = function(movie){
     personalCollectionService.personalCollection.push(movie);
 
     movieService.allMovies.splice(movieService.allMovies.indexOf(movie), 1);
-   
+    $scope.show = false;
+  }
+
+  $scope.remove = function($index){
+    movieService.allMovies.push($index);
+    personalCollectionService.personalCollection.splice($index,1);
+    console.log (personalCollectionService.personalCollection)
+
+    if (!personalCollectionService.personalCollection[0]){
+      console.log($scope.show)
+      $scope.show=true;
+    }
   }
 // });
 
@@ -15,6 +28,8 @@ app.controller('movieCtrl', function($scope, movieService, personalCollectionSer
 // app.controller('collectionCtrl', function($scope, personalCollectionService, yearService){
   $scope.myCollection = personalCollectionService.personalCollection;
   $scope.years = yearService.years;
+
+
   
 
 });
