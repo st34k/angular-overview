077dc05f7427a15315695354d94eb5042a4452ab
Sashko <Sashko>
2016-08-04 11:46:23 +0300

    added budget

--- anguflix/controller.js
+++ anguflix/controller.js
@@ -3,31 +3,34 @@ app.controller('movieCtrl', function($scope, movieService, personalCollectionSer
   $scope.show = true;
   $scope.allMovies = movieService.allMovies;
   $scope.myCollection = personalCollectionService.personalCollection;
+  $scope.years = yearService.years;
+  $scope.budget = 9;
   
 
   $scope.addToCollection = function(movie){
+
+    if ($scope.budget < 3){
+      alert("You don't have enough budget to add more movies!")
+    }
+    else {
     $scope.myCollection.push($scope.allMovies[movie]);
     $scope.allMovies.splice(movie, 1);
     $scope.show = false;
+    $scope.budget-=3;
+  }
+   
+
   }
 
   $scope.remove = function(movie){
     console.log($scope.myCollection[movie])
     $scope.allMovies.push($scope.myCollection[movie]);
     $scope.myCollection.splice(movie,1);
+    
 
     if (!personalCollectionService.personalCollection[0]){
       $scope.show=true;
     }
   }
-// });
-
-
-// app.controller('collectionCtrl', function($scope, personalCollectionService, yearService){
-  
-  $scope.years = yearService.years;
-
-
-  
 
 });
